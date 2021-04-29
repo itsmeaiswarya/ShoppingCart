f5ec56b0a3a3d32439fcf0d0fe043dd6e080a4d5
itsmeaiswarya <69300687+itsmeaiswarya@users.noreply.github.com>
2021-04-29 14:02:04 +0530

    Update viewCart.php

--- viewCart.php
+++ viewCart.php
@@ -2,27 +2,19 @@
 	include "config.php";
 	session_start();
 	$abc = $_SESSION["cartDB"];
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Php Cart</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  
 </head>
 <body>
   
 <div class="container">
   <div class="row">
-			<h1>Cart Items</h1><hr>
+	  <h1><center>Cart Items</center></h1>
 			<a href='view.php'>Home</a>
 			<table class='table'>	
 				<tr>
